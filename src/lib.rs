1244f5a00e0234d757aae7111505311bdc3242b1
immortal infidel <immortal.infidel@gmail.com>
2020-11-28 21:32:25 +0545

    0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![feature(external_doc)]
+
 use ta_common::traits::Indicator;
+
 #[doc(include = "../README.md")]
 pub struct Wilders {
     period: u32,
-    prev: f64,
+    prev_avg: f64,
     sum: f64,
     index: u32,
 }
@@ -12,7 +14,7 @@ impl Wilders {
     pub fn new(period: u32) -> Wilders {
         Self {
             period,
-            prev: 0.0,
+            prev_avg: 0.0,
             sum: 0.0,
             index: 0,
         }
@@ -22,7 +24,7 @@ impl Wilders {
 impl Indicator<f64, Option<f64>> for Wilders {
     fn next(&mut self, input: f64) -> Option<f64> {
         self.index = self.index + 1;
-        println!("index {} sum {}",self.index,self.sum);
+        println!("index {} sum {}", self.index, self.sum);
 
         if self.index < self.period {
             self.sum = self.sum + input;
@@ -30,19 +32,22 @@ impl Indicator<f64, Option<f64>> for Wilders {
         }
         if self.index == (self.period) {
             self.sum = self.sum + input;
-            self.prev = self.sum / self.period as f64;
-            println!("index {} sum {}",self.index,self.sum);
-            Some(self.prev);
+            let res = self.sum / self.period as f64;
+            println!("index {} sum {}", self.index, self.sum);
+            self.prev_avg = res;
+            return Some(res);
         }
-        self.sum = self.sum - self.prev + input;
-        self.prev = self.sum / self.period as f64;
-        Some(self.prev)
+
+        let period = self.period as f64;
+        let res = (self.prev_avg * (period - 1.0) + input) / period;
+        self.prev_avg = res;
+        Some(res)
     }
 
     fn reset(&mut self) {
-        self.prev = 0.0;
         self.sum = 0.0;
         self.index = 0;
+        self.prev_avg = 0.0;
     }
 }
 
@@ -58,16 +63,16 @@ mod tests {
         assert_eq!(wilders.next(81.06), None);
         assert_eq!(wilders.next(82.87), None);
         assert_eq!(wilders.next(83.00), None);
-        assert_eq!(wilders.next(83.61), Some(82.6628));
-        assert_eq!(wilders.next(83.15), Some(82.76024));
-        assert_eq!(wilders.next(82.84), Some(82.776192));
-        assert_eq!(wilders.next(83.99), Some(83.0189536));
-        assert_eq!(wilders.next(84.55), Some(83.32516288000001));
-        assert_eq!(wilders.next(84.36), Some(83.532130304));
-        assert_eq!(wilders.next(85.53), Some(83.93170424320002));
-        assert_eq!(wilders.next(86.54), Some(84.45336339456001));
-        assert_eq!(wilders.next(86.89), Some(84.940690715648));
-        assert_eq!(wilders.next(87.77), Some(85.5065525725184));
-        assert_eq!(wilders.next(87.29), Some(85.86324205801472));
+        assert_eq!(wilders.next(83.61), Some(82.426));
+        assert_eq!(wilders.next(83.15), Some(82.5708));
+        assert_eq!(wilders.next(82.84), Some(82.62464));
+        assert_eq!(wilders.next(83.99), Some(82.897712));
+        assert_eq!(wilders.next(84.55), Some(83.2281696));
+        assert_eq!(wilders.next(84.36), Some(83.45453568));
+        assert_eq!(wilders.next(85.53), Some(83.86962854400001));
+        assert_eq!(wilders.next(86.54), Some(84.40370283520001));
+        assert_eq!(wilders.next(86.89), Some(84.90096226816));
+        assert_eq!(wilders.next(87.77), Some(85.474769814528));
+        assert_eq!(wilders.next(87.29), Some(85.83781585162241));
     }
 }
